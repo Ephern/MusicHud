68da3009dfeef3b48fcf62d3c6bc0d9faf9bc28c
Etern-34520 <a_34520@163.com>
2026-07-13 14:08:13 +0800

    Refine previous merge

--- common/src/main/java/indi/etern/musichud/mixin/LevelLoadingScreenMixin.java
+++ common/src/main/java/indi/etern/musichud/mixin/LevelLoadingScreenMixin.java
@@ -2,18 +2,18 @@
 package indi.etern.musichud.mixin;
 
 import indi.etern.musichud.client.ui.hud.HudRendererManager;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.ReceivingLevelScreen;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.LevelLoadingScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ReceivingLevelScreen.class)
-public class ReceivingLevelScreenMixin {
-    @Inject(method = "render",
+@Mixin(LevelLoadingScreen.class)
+public class LevelLoadingScreenMixin {
+    @Inject(method = "extractRenderState",
             at = @At("RETURN"))
-    private void render(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci) {
+    private void render(GuiGraphicsExtractor guiGraphics, int i, int j, float f, CallbackInfo ci) {
         guiGraphics.nextStratum();
         HudRendererManager.getInstance().renderFrame(guiGraphics, null);
     }
